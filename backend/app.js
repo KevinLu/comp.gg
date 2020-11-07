bee04624eb616cb864064c57d176a4bdd62a9de7
Kevin Lu <18564130+KevinLu@users.noreply.github.com>
2020-11-07 16:47:10 -0500

    Base MERN Stack code

---
+++ backend/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const testRouter = require('./routes/test');
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'));
+  });
+}
+
+app.use('/test', testRouter);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server listening on ${port}.`);
+});
+
+module.exports = app;
---
+++ backend/routes/test.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  return res.status(200).json({success: true, msg: "NICE"});
+});
+
+module.exports = router;
