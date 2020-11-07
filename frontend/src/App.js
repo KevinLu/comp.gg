76f2b6078b78e4ceab2bf46a8ddcc0fd3827d64c
Kevin Lu <18564130+KevinLu@users.noreply.github.com>
2020-11-07 18:19:16 -0500

    Add Homepage view

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-import { ThemeProvider } from "@chakra-ui/core";
-import { theme } from "@chakra-ui/core";
+import {CSSReset, ThemeProvider} from "@chakra-ui/core";
+import {theme} from "@chakra-ui/core";
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+import HomePage from './views/HomePage';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-        </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-        </a>
-        </header>
-      </div>
+      <CSSReset />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+        </Switch>
+      </Router>
     </ThemeProvider>
   );
 }
---
+++ frontend/src/views/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div>
+      apsdhjlashdljk akjahskdhaksdkjasd
+    </div>
+  );
+}
+
+export default HomePage
