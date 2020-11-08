26e8178f1b62591983fbbac3fd90be55633e73fb
Michael Le <le.michael88@gmail.com>
2020-11-07 22:23:40 -0500

    added all riot api callout services

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const Axios = require('axios');
 const generateRiotAPIUrl = require('../services/generateRiotAPIUrl');
+const getSummonerData = require('../services/getSummonerData');
+const getChampionMasteries = require('../services/getChampionMasteries');
+const getMatchHistory = require('../services/getMatchHistory');
 
 router.get('/', (req, res) => {
   return res.status(200).json({success: true, msg: "NICE"});
@@ -18,15 +21,27 @@ router.get('/riot', async (req, res) => {
   }
 });
 
+router.get('/url', async (req, res) => {
+  const data = await getSummonerData(req.query.region, req.query.summonerName);
+  return res.status(200).json({success: true, data: data});
+});
+
+router.get('/url2', async (req, res) => {
+  const summonerData = await getSummonerData(req.query.region, req.query.summonerName);
+  const id = summonerData.accountId;
+  const data = await getMatchHistory(req.query.region, id);
+  return res.status(200).json({success: true, data: data});
+});
+
 router.get('/summonerIcon', async (req, res) => {
-  
-  const url = generateRiotAPIUrl(req.query.region, req.query.summonerName);
-  console.log(url);
-  const response = await Axios.get(url);
-  const iconId = response.data.profileIconId;
-  const iconURL = "http://ddragon.leagueoflegends.com/cdn/10.22.1/img/profileicon/" + iconId + ".png";
-  
-  return res.status(200).json({success: true, data: iconURL});
+  try {
+    const response = await Axios.get(url);
+    const iconId = response.data.profileIconId;
+    const iconURL = "http://ddragon.leagueoflegends.com/cdn/10.22.1/img/profileicon/" + iconId + ".png";
+    return res.status(200).json({success: true, data: iconURL});
+  } catch (error) {
+    return res.status(200).json({success: false, data: "Summoner name does not exist."});
+  }
 });
 
 module.exports = router;
---
+++ backend/services/getMatchHistory.js
@@ -0,0 +1,12 @@
+const Axios = require('axios');
+const generateMatchHistoryUrl = require('./generateMatchHistoryUrl');
+
+// A function that generates the Riot API url for champion masteries
+const getMatchHistory = async (region, accountId) => {
+  const url = generateMatchHistoryUrl(region, accountId);
+  const response = await Axios.get(url);
+  
+  return response.data;
+}
+
+module.exports = getMatchHistory
